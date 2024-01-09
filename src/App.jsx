044c1a74ed5ad7ab236b6b99aa43543ce0acf3de
Tahmid-kabir-addin <tahmidkabiraddin@gmail.com>
2024-01-09 23:10:26 +0600

    Initial Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,3 @@
 export default function App() {
-  return (
-    <>
-      <nav className="py-6">
-        <div className="container mx-auto flex items-center justify-between gap-x-6">
-          <a href="/">
-            <img
-              className="h-[40px]"
-              src="./dist/assets/lws-logo-en.svg"
-              alt="Lws"
-            />
-          </a>
-
-          <a className="px-5 py-2 bg-[#172227] rounded-[44px] text-white" href="#" >
-            Get Admission
-          </a>
-        </div>
-      </nav>
-    </>
-  );
+	return <h1 className="text-xl text-clifford text-center">Hello Assignment!</h1>
 }
